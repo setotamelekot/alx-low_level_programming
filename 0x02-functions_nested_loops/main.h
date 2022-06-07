8a24f46707a4ca5c344a7f3ca2ce8568c831db7c
setotamelekot <setotamelekot00@gmail.com>
2022-06-07 09:17:20 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,33 @@
-0-putchar.c
+#include "setotamelekot.h"                                                                                                          
+
+/**                                                                                                                                 
+ *
+ *  * main - prints the string "Setotamelekot" from a character array.                                                                 
+ *
+ *   * Return: 0 on success.                                                                                                            
+ *
+ *    */                                                                                                                                 
+
+int main(void)                                                                                                                      
+
+{                                                                                                                                   
+
+	        char c[] = "Setotamelekot";                                                                                                 
+
+		        int i;                                                                                                                      
+
+			                                                                                                                                    
+
+			        for (i = 0; i < 9; i++)                                                                                                     
+
+					        {                                                                                                                           
+
+							                _putchar(c[i]);                                                                                                     
+
+									        }                                                                                                                           
+
+				        _putchar('\n');                                                                                                             
+
+					        return (0);                                                                                                                 
+
+} 
