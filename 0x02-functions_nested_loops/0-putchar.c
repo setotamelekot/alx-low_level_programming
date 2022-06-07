7c909f3faa0eb6bc611c7a150d06d86923a2d143
setotamelekot <setotamelekot00@gmail.com>
2022-06-07 09:00:58 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "setotamelekot.h"
+/**
+ * main - prints the string "Setotamelekot" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Setotamelekot";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
