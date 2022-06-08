755b9ce8466911df86996e39d3d22bc8fa75572c
setotamelekot <setotamelekot00@gmail.com>
2022-06-07 18:48:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"                                                                                                     
-
-/**                                                                                                                    * main - prints putchar                                                                                               
-* Return: always 0
-*/                    
-int main (void)                                                                                                        
+#include "main.h"
+/**
+ * main - prints putchar
+ * Return: always 0 
+ */
+int main (void) 
 {
-	        _putchar('_');                                                                                                 
-		         _putchar('p');                                                                                                
-			          _putchar('u');                                                                                                
-				           _putchar('t');                                                                                                
-					            _putchar('c');                     
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n');
 
-						             _putchar('h');                                                                                             
-							              _putchar('a');                                                                                                
-								         _putchar('\n');                                                                                                        
-
-										       return (0);                                                                                                            
-
+	return (0);
 }
