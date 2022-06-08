278011ef41303b2f57d32e8ed17b99f589664892
setotamelekot <setotamelekot00@gmail.com>
2022-06-07 19:47:46 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ */
+
+void print_alphabet(void)
+{ 
+	int j;
+	int x;
+
+	for (x = 0;  x < 10; x++)
+	{
+		for (j = 'a';  j <= 'z'; j++)
+	{
+		 _putchar(j);
+	}
+	 _putchar('\n');
+	}
+}
