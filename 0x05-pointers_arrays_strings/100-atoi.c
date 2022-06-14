2a9556f8795c89a0348b33ca6c17d30c1dfcaf90
setotamelekot <setotamelekot00@gmail.com>
2022-06-13 23:36:51 -0700

    no no

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ * Return: The integer converted to
+ */
+
+int _atoi(char *s)
+{
+	int num, i, negative;
+
+	negative = 1;
+	i = num = 0;
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			negative = negative * -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (num >= 0)
+		{
+			num = num * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			num = num * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	negative = negative * -1;
+	return (num * negative);
+}
