48ffbfbb667c523be36a2e7cb96b651087a0a97c
setotamelekot <setotamelekot00@gmail.com>
2022-06-16 02:51:49 -0700

    taskk

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * infinite_add - ++
+ * @n1: ++
+ * @n2: ++
+ * @r: ++
+ * @size_r: ++
+ * return: ++
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, lenS = 0;
+
+	while (*(n1 + len1++))
+		;
+	while (*(n2 + len2++))
+		;
+	printf("%d, %d\n", len1, len2);
+	return (r);
+}
