e84e7fad652a1aa5da6eed432cad64843a5139ac
setotamelekot <setotamelekot00@gmail.com>
2022-06-22 08:55:19 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - ++
+ * @s: ++
+ * @b: ++
+ * @n: ++
+ * Return: ++
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
