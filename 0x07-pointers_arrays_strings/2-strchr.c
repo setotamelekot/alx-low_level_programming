c88ceee3d88d0e83f637e106373eb6ef496e6770
setotamelekot <setotamelekot00@gmail.com>
2022-06-22 11:41:00 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - ++
+ *  @s: ++
+ *  @c: ++
+ *
+ *  Return: ++
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
