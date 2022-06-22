7f610341fec5a0f3663cbc3e5638fc404b2d0dfd
setotamelekot <setotamelekot00@gmail.com>
2022-06-22 09:19:35 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
+	a = 0;
 
-	for (a = 0; a < n; a++)
+	while (i < n)
 	{
 		dest[a] = src[a];
+		a++;
 	}
-
 	return (dest);
 }
