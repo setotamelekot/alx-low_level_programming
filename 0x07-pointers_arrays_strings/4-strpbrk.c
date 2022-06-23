bcf6a005c1b06e59ad56527e067af46334aca90a
setotamelekot <setotamelekot00@gmail.com>
2022-06-23 09:24:20 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+ /**
+  * _strpbrk - ++
+  * @s: ++
+  *  @accept: ++
+  *  Return: ++
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
