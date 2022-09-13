b8aee787ac5742180f8c11cb768431cfd5b3c03f
setotamelekot <setotamelekot00@gmail.com>
2022-09-13 21:27:12 +0300

    hobston

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,18 @@
  */
 int print_last_digit(int n)
 {
+	if (n < 0)
+		n = n * -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
+/**
+ * print_last_digit - prints the last digit of a numer.
+ * @n: digit to find the last place of.
+ * Return: The last digit.
+ 
+int print_last_digit(int n)
+{
 	int x;
 
 	if (n < 0)
@@ -14,3 +26,5 @@ int print_last_digit(int n)
 	_putchar(x + '0');
 	return (x);
 }
+*/
+
