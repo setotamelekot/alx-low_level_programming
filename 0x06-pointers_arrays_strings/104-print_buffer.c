f78f92fc2a7c88ab2f975b4f119b8a02d2cbc1ea
setotamelekot <setotamelekot00@gmail.com>
2022-09-22 22:33:40 +0300

    my final fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,9 +11,9 @@
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
-
+	
 	o = 0;
-
+	
 	if (size <= 0)
 	{
 		printf("\n");
@@ -37,12 +37,14 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
-
+			
 			if (c < 32 || c > 132)
 			{
 				c = '.';
 			}
-			printf("\n");
-			o += 10;
+			printf("%c", c);
 		}
+		printf("\n");
+		o += 10;
 	}
+}
