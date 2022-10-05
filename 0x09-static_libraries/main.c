6dcf27147c0170b0bd0998136c5796c9dc252627
setotamelekot <setotamelekot00@gmail.com>
2022-10-05 14:26:40 -0700

    fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"Attheendoftheday,mygoalwastobethebesthacker\"\n\t- KevinMitnick");
 return (0);
 }
